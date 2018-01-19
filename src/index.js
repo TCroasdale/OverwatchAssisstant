78bd15248c829adb6ae0ed8bddd7a9df63fa268b
Tom Croasdale <tecroasdale1@sheffield.ac.uk>
2018-01-19 21:07:30 +0000

    Assistant can give you your level and SR information

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-//var request = require('request')
+var request = require('request')
+
+/**
+ * platform := pc, psn or xbl
+ * region := us, eu, etc
+ * player := battle-net, psn, or xbl name of player
+ */
+function Profile(){
+    this.platform = "psn"
+    this.region = "eu"
+    this.player = "Tcr04sd1zzle"
+}
 
 exports.handler = function( event, context ){	
 
@@ -7,17 +18,17 @@ exports.handler = function( event, context ){
 		sessionAttributes = event.session.attributes
 	
 	
-    if (event.request.type === "LaunchRequest") { //If Docker is launcher without intent.
+    if (event.request.type === "LaunchRequest") { //If OWA is launcher without intent.
         var out = "Hello, I am overwatch assistant. how may I help?"
         context.succeed({response: buildSpeechletResponse(out, false)})
-    }else if(event.request.type === "SessionEndedRequest"){ //If docker is exited by saying "quit"
-		var out = "Grab that napkin, you just got served"
+    }else if(event.request.type === "SessionEndedRequest"){ //If OWA is exited by saying "quit"
+		var out = "Heroes never die"
 		context.succeed({response: buildSpeechletResponse(out, true)})
 	}else{ //If a command is given
 		var IntentName = event.request.intent.name
         
         if(IntentName == "GetRankIntent"){
-
+            getRank(context)
         }else if(IntentName == "GetMainIntent"){
 
         }else if(IntentName == "GetCharacterStatsIntent"){
@@ -32,29 +43,69 @@ exports.handler = function( event, context ){
 	}
 }
 
-/**
- * Makes a request to https://ow-api.com/v1/stats/
- * @param {*} platform pc, psn or xbl
- * @param {*} region us, eu, etc
- * @param {*} player battle-net, psn, or xbl name of player
- * @param {*} request The request to make
- */
-function makeRequestToAPI(platform, region, player, request){
-    
+function getRank(context){
+    makeRequestToAPI(new Profile(), 'profile', function(res){
+        
+        var sr = res.rating
+        var level = res.level
+        var prestige = res.prestige
+
+        var tts = "Your S.R. is " + sr + ", your level is " + level + ", prestige " + prestige
+        if(sr == ""){
+            tts = "You currently have no competitive rank, or S.R. your level is " + level + ", prestige " + prestige
+        }
+
+        context.succeed({response: buildSpeechletResponse(tts, false)}) 
+    })
 }
 
+/**
+ * Makes a request to https://ow-api.com/v1/stats/
+ */
+function makeRequestToAPI(profile, apiReq, callback){
+    var nameOK = true
+    var reqOK = true
+    var serverOK = true
+    var json = {}
 
-// request('https://docker.jhc.pw/containers', function (error, response, body) { //outputs the number of containers
-//     if (!error && response.statusCode == 200) {
-//         var parsedJson = JSON.parse(body) //parses the JSON
-//         var output = "there are " + parsedJson.running_containers +" containers running, out of a total of " + parsedJson.total_containers + "."
-        
-//         context.succeed({response: buildSpeechletResponse(output, false)}) //tell alexa to say the repsonse, and carry on.
-//     }
-//     else{
-//         context.succeed({response: buildSpeechletResponse("I cannae dew et cap'n", false)})//If the request fails (server is down).				
-//     }
-// })		
+    request(getURL(profile, apiReq), function(error, res, body){
+        // if(error){
+        //     switch(res.statusCode){
+        //         case 400:
+        //             reqOK = false
+        //             break
+        //         case 404:
+        //             nameOK = false
+        //             break
+        //         case 406:
+        //             reqOK = false
+        //             break
+        //         case 500:
+        //             serverOK = false
+        //             break
+        //         case 503:
+        //             serverOK = false
+        //             break
+        //     }
+        // }else{
+        if(!error && res.statusCode == 200){
+            console.log(body)
+            json = JSON.parse(body)
+        }
+        callback(json)//{nameOK: nameOK, requestOK: reqOK, serverOK: serverOK, response: json})
+    })
+}
+
+function getURL(prf, req){
+    var url = 'https://ow-api.com/v1/stats/' + prf.platform + '/'
+    url += prf.region + '/'
+    url += prf.player + '/'
+    url += req
+
+    ///return 'https://ow-api.com/v1/stats/'
+    console.log(url)
+    return url
+}
 
 function buildSpeechletResponse(say, shouldEndSession) {
     return {
