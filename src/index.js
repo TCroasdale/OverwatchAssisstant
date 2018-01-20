464386070e00828ed7fd67656d81529c4d136c7d
Tom Croasdale <tecroasdale1@sheffield.ac.uk>
2018-01-20 21:47:32 +0000

    Character stat requests can now be made

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ function Profile(){
     this.player = "Tcr04sd1zzle"
 }
 
+
 exports.handler = function( event, context ){	
 
 	var sessionAttributes = {} //variables stored from the last command.
@@ -29,30 +30,54 @@ exports.handler = function( event, context ){
         
         if(IntentName == "GetRankIntent"){
             getRank(context)
-        }else if(IntentName == "GetMainIntent"){
+        }
+        else if(IntentName == "GetMainIntent"){
 
-        }else if(IntentName == "GetCharacterStatsIntent"){
-
-        }else if(IntentName === "HelpIntent"){ //If help is asked for
+        }
+        else if(IntentName == "GetCharacterStatsIntent"){
+            var hero = parseHeroSlot(event.request.intent.slots.heroName.value)
+            getCharacterStats(context, hero);
+        }
+        else if(IntentName === "HelpIntent"){ //If help is asked for
 			var out = "try asking about your favourite character."
 			context.succeed({response: buildSpeechletResponse(out, false)})
-		}else if(IntentName === "QuitIntent"){ //If the session is closed
+        }
+        else if(IntentName === "QuitIntent"){ //If the session is closed
 			var out = "Heroes Never Die"
 			context.succeed({response: buildSpeechletResponse(out, true)})
 		}		
 	}
 }
 
+/**
+ * Processes a getRankIntent request
+ * @param {*} context The amazon alexa context.
+ */
 function getRank(context){
     makeRequestToAPI(new Profile(), 'profile', function(res){
         
-        var sr = res.rating
-        var level = res.level
-        var prestige = res.prestige
+        var sr = res.response.rating
+        var level = res.response.level
+        var prestige = res.response.prestige
+        var rank = res.response.ratingName
 
-        var tts = "Your S.R. is " + sr + ", your level is " + level + ", prestige " + prestige
-        if(sr == ""){
-            tts = "You currently have no competitive rank, or S.R. your level is " + level + ", prestige " + prestige
+        var tts = "Your rank is " + rank + " , with " + sr + " sr. you are level " + level + ", prestige " + prestige
+        if(sr == ""){ //sr="" if there is no rank for this season.
+            tts = "You currently have no competitive rank. you are level  " + level + ", prestige " + prestige
+        }
+
+        //Making sure the data fetched was valid.
+        if(!res.nameOK || !res.requestOK || !res.serverOK){
+            tts = "There was a problem fetching that information."
+            if(!res.nameOK){
+                tts += " The name you provded wasn't valid."
+            }
+            if(!res.requestOK){
+                tts += " This was my fault, I made a bad request."
+            }
+            if(!res.serverOK){
+                tts += " there was a problem with the A.P.I servers"
+            }
         }
 
         context.succeed({response: buildSpeechletResponse(tts, false)}) 
@@ -60,7 +85,62 @@ function getRank(context){
 }
 
 /**
- * Makes a request to https://ow-api.com/v1/stats/
+ * processes a getCharacterStatsIntent request
+ * @param {Alexa Context} context The alexa context provided 
+ * @param {string} hero The Hero name (must be the API version) 
+ */
+function getCharacterStats(context, hero){
+    makeRequestToAPI(new Profile(), 'heroes/'+hero, function(res){
+        var tts = ""
+        //Making sure the data fetched was valid.
+        if(!res.nameOK || !res.requestOK || !res.serverOK){
+            tts = "There was a problem fetching that information."
+            if(!res.nameOK){
+                tts += " The name you provded wasn't valid."
+            }
+            if(!res.requestOK){
+                tts += " This was my fault, I made a bad request."
+            }
+            if(!res.serverOK){
+                tts += " there was a problem with the A.P.I servers"
+            }
+        }
+        else{
+            var compStats = res.response.competitiveStats.topHeroes[hero]
+            var qpStats = res.response.quickPlayStats.topHeroes[hero]
+
+            if(compStats.timePlayed != "--"){
+                tts = "In competitive: " + statsToSentence(compStats)
+            }else if(qpStats.timePlayed != "--"){
+                tts = "In competitive: " + statsToSentence(qpStats)
+            }else{
+                tts = "You haven't played as " + hero
+            }
+        }
+
+        context.succeed({response: buildSpeechletResponse(tts, false)}) 
+    })
+}
+
+/**
+ * Converts JSON hero stats to an english sentence
+ * @param {JSON object of hero stats} statsJS 
+ */
+function statsToSentence(statsJS){
+    var sentence = "You have played for " + statsJS.timePlayed
+    sentence += ". won: " + statsJS.gamesWon + " games, thats a " + statsJS.winPercentage
+    sentence += " percent win rate."
+    sentence += " you normally get: " + statsJS.eliminationsPerLife + " eliminations per life, your best multikill: is"
+    sentence += statsJS.multiKillBest + " kills. your accuracy is: " + statsJS.weaponAccuracy + " percent."
+    
+    return sentence
+}
+
+/**
+ * Makes a request to the API.
+ * @param {Profile} profile The profile of the user 
+ * @param {string} apiReq The request to make
+ * @param {function(jsonresponse)} callback The callback function, called once request has been returned
  */
 function makeRequestToAPI(profile, apiReq, callback){
     var nameOK = true
@@ -68,45 +148,66 @@ function makeRequestToAPI(profile, apiReq, callback){
     var serverOK = true
     var json = {}
 
-    request(getURL(profile, apiReq), function(error, res, body){
-        // if(error){
-        //     switch(res.statusCode){
-        //         case 400:
-        //             reqOK = false
-        //             break
-        //         case 404:
-        //             nameOK = false
-        //             break
-        //         case 406:
-        //             reqOK = false
-        //             break
-        //         case 500:
-        //             serverOK = false
-        //             break
-        //         case 503:
-        //             serverOK = false
-        //             break
-        //     }
-        // }else{
+    request(getURL(profile, apiReq), function(error, res, body){   
         if(!error && res.statusCode == 200){
             console.log(body)
             json = JSON.parse(body)
+        }else if(!error){
+            switch(res.statusCode){
+                case 400:
+                    reqOK = false
+                    break
+                case 404:
+                    nameOK = false
+                    break
+                case 406:
+                    reqOK = false
+                    break
+                case 500:
+                    serverOK = false
+                    break
+                case 503:
+                    serverOK = false
+                    break
+            }
         }
-        callback(json)//{nameOK: nameOK, requestOK: reqOK, serverOK: serverOK, response: json})
+        callback({nameOK: nameOK, requestOK: reqOK, serverOK: serverOK, response: json})
     })
 }
 
+/**
+ * constructs and returns the URL you need.
+ * @param {Profile} prf The profile object for the user 
+ * @param {string} req the end of the url
+ */
 function getURL(prf, req){
     var url = 'https://ow-api.com/v1/stats/' + prf.platform + '/'
     url += prf.region + '/'
     url += prf.player + '/'
     url += req
 
-    ///return 'https://ow-api.com/v1/stats/'
-    console.log(url)
     return url
 }
 
+/**
+ * Returns the correct version of a hero name for the API
+ * @param {string} hero The name of the hero, from hero slot
+ */
+function parseHeroSlot(hero){
+    if(hero == 'diva'){
+        return 'dVa'
+    }
+    else if(hero == 'soldier 76'){
+        return 'soldier76'
+    }
+    return hero;
+}
+
+/**
+ * returns the json which makes alexa talk.
+ * @param {string} say What alexa should say
+ * @param {bool} shouldEndSession whether or not alexa should close OWA
+ */
 function buildSpeechletResponse(say, shouldEndSession) {
     return {
         outputSpeech: {
